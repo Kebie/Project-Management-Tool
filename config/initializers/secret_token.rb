da2c84d41a1a37d7c1d360aeda43ed14abe0eb10
Kebie <nigel.kehler@gmail.com>
2014-03-24 22:38:34 -0700

    initial commit, rails enviroment setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PmTool::Application.config.secret_key_base = '77138fc41aab73f59844595687b38e6e83823e8c4c0b5588a84f564bfb70b00a526e5c08850d4d5ea4ae98729c116aa40c78fc5cd3f9014e5e1ba894d4203f18'
